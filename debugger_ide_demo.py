b3e7a043e3001e770823e2ba47583b5460f5a62b
btthl <davin.nguyen19@gmail.com>
2024-05-23 10:43:28 -0700

    remove fibonacci thing

--- debugger_ide_demo.py
+++ debugger_ide_demo.py
@@ -1,20 +1,3 @@
-
-class fibonacci_counter():
-    start_1 = 0
-    start_2 = 1
-
-    def __init__(self) -> None:
-        self.num_1 = fibonacci_counter.start_1
-        self.num_2 = fibonacci_counter.start_2
-        pass
-
-    def next(self) -> int:
-        temp = self.num_1
-        self.num_1 = self.num_2
-        self.num_2 = temp + self.num_2
-        return temp
-
-
 def add_nums(a : int, b : int) -> int:
     return a + b
 
@@ -24,7 +7,7 @@ if __name__ == "__main__":
     sum = add_nums(num_1, num_2)
     print("sum: " + str(sum))
 
-    counter = fibonacci_counter()
     for i in range(10):
-        print("fib " + str(i + 1) + ": " + str(counter.next()))
+        loop_sum = add_nums(i, i + 1)
+        print(loop_sum)
     pass
