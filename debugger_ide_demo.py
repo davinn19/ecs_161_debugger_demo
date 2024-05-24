425ccaf698205400a4e8bd13f54cbdd7a4605cb6
btthl <davin.nguyen19@gmail.com>
2024-05-24 14:23:37 -0700

    added code to demonstrate call stack window

--- debugger_ide_demo.py
+++ debugger_ide_demo.py
@@ -3,9 +3,11 @@ def add_nums(a : int, b : int) -> int:
 
 if __name__ == "__main__":
     num_1 = 1
-    num_2 = 2
-    sum = add_nums(num_1, num_2)
-    print("sum: " + str(sum))
+    num_1 = 2
+    num_2 = 3
+
+    sum_1 = add_nums(num_1, num_2)
+    sum_2 = add_nums(num_1, add_nums(num_1, 3))
 
     for i in range(10):
         loop_sum = add_nums(i, i + 1)
